b2ebabbb35b46f4435a01c2a293e7869e934532e
https://github.com/RS/Online/GDS/Departmental/Exam/Study/Hub. git <rameshwarsahu2014bpmjarwe1694@gmail.com>
2026-06-12 19:53:59 +0530

    Update sw.js
    Signed-off-by: https://github.com/RS/Online/GDS/Departmental/Exam/Study/Hub. git <rameshwarsahu2014bpmjarwe1694@gmail.com>

--- sw.js
+++ sw.js
@@ -1,38 +1,42 @@
-const CACHE_NAME = "gds-notes-v9";
+const CACHE_NAME = "gds-notes-v10";
 
-// Cache hone wali zaroori files ki list (Optional but recommended)
 const ASSETS = [
   "./",
   "./index.html",
-  "./RSH.png" 
+  "./manifest.json",
+  "./RSH.png",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
 
 self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
   self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
-  // Sirf GET requests handle karein
   if (event.request.method !== "GET") return;
 
   event.respondWith(
-    fetch(event.request)
-      .then((response) => {
-        // Response milne par use cache mein update karein
-        const clone = response.clone();
-        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
-        return response;
-      })
-      .catch(() => {
-        // Network fail hone par cache se lein
-        return caches.match(event.request);
-      })
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
+    })
   );
 });
