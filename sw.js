396728a7da77e5571ec323c9e01a1327ed0dd538
https://github.com/RS/Online/GDS/Departmental/Exam/Study/Hub. git <rameshwarsahu2014bpmjarwe1694@gmail.com>
2026-06-15 21:34:48 +0530

    Update service worker cache name and fetch logic
    Signed-off-by: https://github.com/RS/Online/GDS/Departmental/Exam/Study/Hub. git <rameshwarsahu2014bpmjarwe1694@gmail.com>

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "gds-notes-v14";
+const CACHE_NAME = "gds-notes-v15";
 
 const ASSETS = [
   "./",
@@ -41,8 +41,16 @@ self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
 
   event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request);
-    })
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, copy);
+        });
+
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
